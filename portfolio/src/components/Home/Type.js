dc770c79845e609cabdbca41f0daf0bfe082299f
Shankara-asign <idofshankar@gmail.com>
2025-08-16 18:44:13 +0530

    added

--- portfolio/src/components/Home/Type.js
+++ portfolio/src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Front End Developer",
+       
         ],
         autoStart: true,
         loop: true,
